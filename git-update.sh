bc78e7bf6faa3c167e495387e4303ac4edae9883
Daniel Butum <danibutum@gmail.com>
2020-10-15 16:36:50 +0300

    Add 4.24 project

--- git-update.sh
+++ git-update.sh
@@ -7,5 +7,5 @@ git pull --verbose
 
 # Update subtree
 git subtree pull \
-    --prefix Plugins/DlgSystem https://gitlab.com/NotYetGames/DlgSystem.git 4.23 \
+    --prefix Plugins/DlgSystem https://gitlab.com/NotYetGames/DlgSystem.git 4.24 \
     --squash --message "Update Dialogue Plugin"
